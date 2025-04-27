eb985ef5676c77b637a406bb5a6540d894edf7ec
knife <ashrafnaiefryan@gmail.com>
2025-04-27 16:34:52 +0530

    implemented the 'load more' button

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,35 @@
 import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [skip, setSkip] = useState(12);
+  const limit = 12;
   const [data, setData] = useState();
   const [isPending, setIsPending] = useState(true);
   const [err, setErr] = useState();
 
+  const handleClick = () => {
+    setIsPending(true);
+    fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((newProducts) => {
+        console.log(skip);
+        setSkip(skip + limit);
+        const updatedData = {
+          ...data,
+          products: [...data.products, ...newProducts.products],
+        };
+        setData(updatedData);
+        setIsPending(false);
+        setErr(null);
+        console.log(data);
+      })
+      .catch((err) => console.error(err));
+  };
+
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
+    fetch(`https://dummyjson.com/products?limit=${limit}`)
       .then((result) => {
         console.log("req sent");
         return result.json();
@@ -25,17 +48,19 @@ const Home = () => {
 
   return (
     <div>
-      <h1 class="page-title">OUR AMAZING PRODUCTS</h1>
+      <h1 className="page-title">OUR AMAZING PRODUCTS</h1>
 
-      {isPending && <h3 className="loading">loading...</h3>}
       <div className="product-list">
         {data &&
           data.products.map((product, i) => {
-            fetch("https://dummyjson.com/products").then(console.log("hello"));
-            console.log(product);
+            // console.log(product);
             return (
               <div className="product-container" key={i}>
-                <img className="images" src={product.images[0]} />
+                <img
+                  className="images"
+                  src={product.images[0]}
+                  alt={product.title}
+                />
                 <h2>{product.title}</h2>
                 <div className="product-desc">{product.description}</div>
                 <div className="prices">
@@ -52,8 +77,14 @@ const Home = () => {
           })}
       </div>
 
+      {isPending && <h3 className="loading">loading...</h3>}
+
       <div>
-        {!isPending && <button className="load-button">Load More</button>}
+        {!isPending && (
+          <button onClick={handleClick} className="load-button">
+            Load More
+          </button>
+        )}
       </div>
     </div>
   );
